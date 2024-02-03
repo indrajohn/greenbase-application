94e69616b4eae4b9f34057eba67ccd9b7c906494
Indra John <indrajohn2@gmail.com>
2024-02-03 11:49:43 +1100

    add chart dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { NgChartsModule } from 'ng2-charts';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    NgChartsModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
